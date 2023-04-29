0444134056feeba422f6ffe8cf51e91313cf82f3
Devon McGrath <devon.mcgrath@gmail.com>
2023-04-29 18:34:45 +1200

    Add component layout

---
+++ components/Timer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const Timer = () => {
+    
+
+    return (
+        <div>
+          timer
+        </div>
+    )
+}
+
+export default Timer;
