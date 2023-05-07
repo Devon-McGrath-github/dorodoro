ec77185381aabce616334988dd4c25bb1b1d201f
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-07 18:19:30 +1200

    Add CountdownTimer component

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 
-const Timer = () => {
-    
+// set intiial time
+const INITIAL_TIME: number = 120
 
-    return (
-        <div>
-          timer
-        </div>
-    )
+export default function CountdownTimer() {
+  // useState to set time remaining
+  const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_TIME)
+  
+
+  const secondsToDisplay = secondsRemaining % 60
+  const minutesToDisplay = ( secondsRemaining - secondsToDisplay ) / 60
+  
+
+  // requires use effect,
+
+  useEffect(() => {
+
+    // add if statement so timer stops at 0 
+    const interval = setInterval(() => {
+      setSecondsRemaining(secondsRemaining => secondsRemaining - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  // return seconds left as an element on the page
+
+  return (
+    <div>
+      {/* need to add function to display 2 digits (clock format) */}
+      timer: {minutesToDisplay} : {secondsToDisplay}
+    </div>
+  )
 }
-
-export default Timer;
