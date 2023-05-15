ccb6a02d5efd8fdfda0cd558808181d1906ac04a
Devon McGrath <devon.mcgrath@gmail.com>
2023-05-15 21:58:31 +1200

    Add tailwind styling to buttons

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -44,20 +44,20 @@ export default function CountdownApp() {
         {twoDigits(secondsToDisplay)}
       </div>
 
-      <button onClick={handleStart} type="button">
+      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      onClick={handleStart} type="button">
         Start
       </button>
 
-      <button onClick={handleStop} type="button">
+      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      onClick={handleStop} type="button">
         Stop
       </button>
 
-      <button onClick={handleReset} type="button">
+      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      onClick={handleReset} type="button">
         Reset
       </button>
-
-
-      <div>Status: {status}</div>
     </div>
   )
 }
