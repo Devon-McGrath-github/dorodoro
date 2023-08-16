962aad8846a3ff04332d924cc58116546a2ac3e9
Devon McGrath <devon.mcgrath@gmail.com>
2023-08-16 14:00:17 +1200

    Merge Start/Stop buttons into single button that toggles

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,86 +1,78 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
-const STATUS = {
-  STARTED: 'Started',
-  STOPPED: 'Stopped',
-}
-
-const INITIAL_COUNT = 1500
+const INITIAL_COUNT = 1500;
 
 export default function CountdownApp() {
-  const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT)
-  const [status, setStatus] = useState(STATUS.STOPPED)
+  const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT);
+  const [countingDown, setCountingDown] = useState(false);
 
-  const secondsToDisplay = secondsRemaining % 60
-  const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60
-  const minutesToDisplay = minutesRemaining % 60
+  const secondsToDisplay = secondsRemaining % 60;
+  const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60;
+  const minutesToDisplay = minutesRemaining % 60;
 
-  const handleStart = () => {
-    setStatus(STATUS.STARTED)
-  }
-  const handleStop = () => {
-    setStatus(STATUS.STOPPED)
-  }
+  const handleClick = () => {
+    setCountingDown(!countingDown);
+  };
+
   const handleReset = () => {
-    setStatus(STATUS.STOPPED)
-    setSecondsRemaining(INITIAL_COUNT)
-  }
+    setCountingDown(false);
+    setSecondsRemaining(INITIAL_COUNT);
+  };
   useInterval(
     () => {
       if (secondsRemaining > 0) {
-        setSecondsRemaining(secondsRemaining - 1)
+        setSecondsRemaining(secondsRemaining - 1);
       } else {
-        setStatus(STATUS.STOPPED)
+        setCountingDown(false);
       }
     },
-    status === STATUS.STARTED ? 1000 : null,
+    countingDown ? 1000 : null
     // passing null stops the interval
-  )
+  );
   return (
     <div className="App">
-
       <div>
-        {twoDigits(minutesToDisplay)}:
-        {twoDigits(secondsToDisplay)}
+        {twoDigits(minutesToDisplay)}:{twoDigits(secondsToDisplay)}
       </div>
 
-      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-      onClick={handleStart} type="button">
-        Start
+      <button
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        onClick={handleClick}
+        type="button"
+      >
+        {countingDown ? 'Stop' : 'Start'}
       </button>
 
-      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-      onClick={handleStop} type="button">
-        Stop
-      </button>
-
-      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-      onClick={handleReset} type="button">
+      <button
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        onClick={handleReset}
+        type="button"
+      >
         Reset
       </button>
     </div>
-  )
+  );
 }
 
 // source: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 function useInterval(callback: any, delay: number | null) {
-  const savedCallback = useRef()
+  const savedCallback = useRef();
 
   // Remember the latest callback.
   useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
+    savedCallback.current = callback;
+  }, [callback]);
 
   // Set up the interval.
   useEffect(() => {
     function tick() {
-      savedCallback.current()
+      savedCallback.current();
     }
     if (delay !== null) {
-      let id = setInterval(tick, delay)
-      return () => clearInterval(id)
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
     }
-  }, [delay])
+  }, [delay]);
 }
 
-const twoDigits = (num: number) => String(num).padStart(2, '0')
+const twoDigits = (num: number) => String(num).padStart(2, '0');
