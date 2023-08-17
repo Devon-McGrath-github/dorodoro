f2d77111bedd69576c85e06bda16d9295f9b247d
Devon McGrath <devon.mcgrath@gmail.com>
2023-08-17 14:28:14 +1200

    Add count variable to head as example

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,35 @@ import NavBar from '@/components/NavBar/NavBar';
 import Timer from '@/components/Timer';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  let [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    //Clearing the interval
+    return () => clearInterval(interval);
+  }, [count]);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div>
+        <p>{count}</p>
         <Head>
-          <title> DoroDoro </title>
+          <title> {count} </title>
           <meta property="og:title" content="My page title" key="title" />
         </Head>
+
         <NavBar />
+
         <Timer duration={1500} />
         <Timer duration={900} />
         <Timer duration={300} />
