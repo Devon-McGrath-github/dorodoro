86ad1461042a6de950302dede4b83642bad16fa7
Devon McGrath <devon.mcgrath@gmail.com>
2023-08-17 14:17:07 +1200

    Add Head property from Nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import NavBar from '@/components/NavBar/NavBar';
 import Timer from '@/components/Timer';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,6 +11,10 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div>
+        <Head>
+          <title> DoroDoro </title>
+          <meta property="og:title" content="My page title" key="title" />
+        </Head>
         <NavBar />
         <Timer duration={1500} />
         <Timer duration={900} />
