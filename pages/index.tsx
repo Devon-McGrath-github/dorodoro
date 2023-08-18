58cdb27b2554e6cdc922a96facc81258ae870e7c
Devon McGrath <devon.mcgrath@gmail.com>
2023-08-18 12:29:03 +1200

    Add min-height to body

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import NavBar from '@/components/NavBar/NavBar';
 import Tabs from '@/components/Tabs/Tabs';
-import Timer from '@/components/Timer';
 import Layout from '@/layouts';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
@@ -11,23 +10,12 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   let [count, setCount] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount(count + 1);
-    }, 1000);
-
-    //Clearing the interval
-    return () => clearInterval(interval);
-  }, [count]);
-
   return (
     <Layout>
       <main
         className={`flex flex-col items-center justify-between p-24 ${inter.className}`}
       >
         <div>
-          <p className="mb-2">{count}</p>
-
           <Head>
             <title> {count} </title>
             <meta property="og:title" content="My page title" key="title" />
