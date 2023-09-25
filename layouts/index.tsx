05720e83bf00677f0f190bf6579894e3df2a025f
Devon McGrath <devon.mcgrath@gmail.com>
2023-09-26 11:57:28 +1300

    Remove unused swap code

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -7,12 +7,10 @@ interface LayoutProviderProps {
 
 const inter = Inter({
 	subsets: ['latin'],
-	// display: 'swap',
 	variable: '--font-inter',
 })
 const roboto_mono = Roboto_Mono({
 	subsets: ['latin'],
-	// display: 'swap',
 	variable: '--font-roboto-mono',
 })
 
