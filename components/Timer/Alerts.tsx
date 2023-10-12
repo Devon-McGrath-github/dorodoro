56251fc802ececa14d47921cc0f6797f07dcbd50
Devon McGrath <devon.mcgrath@gmail.com>
2023-10-12 15:51:25 +1300

    Add notification messages to an object
    Should change timer in state to an object, with title, notification message, duration etc. assigned, so that its all derived from one location

--- components/Timer/Alerts.tsx
+++ components/Timer/Alerts.tsx
@@ -2,28 +2,25 @@ import { useStore } from '@/store/Timer'
 import React, { useEffect } from 'react'
 
 export default function Alerts() {
-	const [duration, timer, updateTimer] = useStore((s) => [
-		s.duration,
-		s.timer,
-		s.updateTimer,
-	])
+	const [duration, timer] = useStore((s) => [s.duration, s.timer])
 
-	const text = `Timer Finished`
+	interface NotificationsLayout {
+		default: string
+		shortBreak: string
+		longBreak: string
+	}
 
-	// add logic to change alert based on current timer
-	// if not default
-	//   alert must be sent
-	//   timer must be updated to default
-	//   tab must change to default
-	// else
-	//   must know if its 1st timer, second 3rd or forth to switch to shortbreak or long break
-	//   likely have to update store to do this. New state element, rework tabs page
+	const notifications: NotificationsLayout = {
+		default: 'Time for a Break',
+		shortBreak: 'Time to Work',
+		longBreak: 'Time to Work',
+	}
 
-	// comment out
 	useEffect(() => {
 		if (duration === 0) {
-			console.log('trigger')
-			new Notification('Timer finished', { body: text })
+			new Notification('Times Up', {
+				body: notifications[timer as keyof NotificationsLayout],
+			})
 		}
 		return
 	}, [duration])
