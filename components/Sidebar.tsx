e418f7216c427736d5bd5f31a94c97323685cff9
Devon McGrath <devon.mcgrath@gmail.com>
2023-10-17 15:39:21 +1300

    Tweak sidebar layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,20 +14,22 @@ export default function Sidebar() {
 			</div>
 
 			<div
-				className={`font-sans bg-skin-primary flex flex-col drop-shadow-4xl top-0 right-0 w-[35vw] p-5 text-white absolute  min-h-screen z-40  ease-in-out duration-300 ${
+				className={`font-sans bg-skin-primary flex flex-col gap-4 drop-shadow-4xl top-0 right-0 w-[35vw] p-5 text-white absolute  min-h-screen z-40  ease-in-out duration-300 ${
 					showSidebar ? 'translate-x-0 ' : 'translate-x-full'
 				}`}
 			>
 				<div
 					onClick={() => setShowSidebar(!showSidebar)}
-					className=' flex flex-row justify-end mb-4'
+					className=' flex flex-row justify-end'
 				>
 					<XMarkIcon className='h-10 w-10' />
 				</div>
 
-				<div className='flex flex-row gap-4'>
-					<h3>Colour: </h3>
-					<ColorPicker />
+				<div className='flex flex-col items-center '>
+					<div className='flex flex-row items-center gap-4'>
+						<h2 className='text-2xl font-medium'>Colour: </h2>
+						<ColorPicker />
+					</div>
 				</div>
 			</div>
 		</>
