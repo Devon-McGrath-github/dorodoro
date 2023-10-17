65b78c517eb41cd94446087a029ddbc034fa0197
Devon McGrath <devon.mcgrath@gmail.com>
2023-10-17 14:52:17 +1300

    Add Icons to SideBar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,36 +2,27 @@ import { useState } from 'react'
 import ColorPicker from './ColorPicker'
 
 import React from 'react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Sidebar() {
 	const [showSidebar, setShowSidebar] = useState(false)
 
 	return (
 		<>
-			<svg
-				onClick={() => setShowSidebar(!showSidebar)}
-				className='z-30 items-center cursor-pointer fill-white'
-				viewBox='0 0 100 80'
-				width='40'
-				height='40'
-			>
-				<rect width='100' height='10'></rect>
-				<rect y='30' width='100' height='10'></rect>
-				<rect y='60' width='100' height='10'></rect>
-			</svg>
+			<div onClick={() => setShowSidebar(!showSidebar)}>
+				<Bars3Icon className='h-10 w-10' />
+			</div>
 
 			<div
-				className={`bg-skin-primary flex flex-col drop-shadow-4xl top-0 right-0 w-[35vw] p-5 text-white absolute  min-h-screen z-40  ease-in-out duration-300 ${
+				className={`font-sans bg-skin-primary flex flex-col drop-shadow-4xl top-0 right-0 w-[35vw] p-5 text-white absolute  min-h-screen z-40  ease-in-out duration-300 ${
 					showSidebar ? 'translate-x-0 ' : 'translate-x-full'
 				}`}
 			>
-				<div className='flex flex-row justify-end mb-4'>
-					<button
-						className=' text-4xl text-white cursor-pointer z-50'
-						onClick={() => setShowSidebar(!showSidebar)}
-					>
-						x
-					</button>
+				<div
+					onClick={() => setShowSidebar(!showSidebar)}
+					className=' flex flex-row justify-end mb-4'
+				>
+					<XMarkIcon className='h-10 w-10' />
 				</div>
 
 				<div className='flex flex-row gap-4'>
