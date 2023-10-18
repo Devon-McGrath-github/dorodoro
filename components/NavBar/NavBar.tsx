cafe8c3fd52e1088cd2fdc41ab92e77cd14f8652
Devon McGrath <devon.mcgrath@gmail.com>
2023-10-18 14:56:04 +1300

    Update responsiveness and colors of Nav + Main content

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import Sidebar from '../Sidebar'
 
 const NavBar = () => {
 	return (
-		<nav className='fixed w-full flex flex-row items-center justify-between p-5'>
+		<nav className='fixed w-full bg-skin-primary flex flex-row items-center justify-between p-5'>
 			<Link className='font-sans' href='/'>
 				DoroDoro
 			</Link>
