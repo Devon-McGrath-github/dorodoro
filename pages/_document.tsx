2a2884238b9c4f4232f5e43028feef42b7c7a192
Devon-McGrath-github <devon.mcgrath@gmail.com>
2023-10-21 14:11:12 +1300

    Try Google analytics directly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 const GOOGLE_TAG_MANAGER_ID = 'G-HZ5FVTE39B'
 
@@ -6,27 +7,28 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-						})(window,document,'script','dataLayer', '${GOOGLE_TAG_MANAGER_ID}');
-            `,
-					}}
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_MANAGER_ID}`}
 				/>
+				<Script id='google-analytics'>
+					{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GOOGLE_TAG_MANAGER_ID}');
+        `}
+				</Script>
 			</Head>
 			<body className='min-h-screen text-white bg-skin-primary bg-opacity-70'>
-				<noscript
+				{/* <noscript
 					dangerouslySetInnerHTML={{
 						__html: `
               <iframe src="https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
 					}}
-				/>
+				/> */}
 				<Main />
 				<NextScript />
 			</body>
