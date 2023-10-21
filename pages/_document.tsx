d46d416b8d57fd94c6c9a8873d31980899c90bc6
Devon-McGrath-github <devon.mcgrath@gmail.com>
2023-10-21 15:15:03 +1300

    Revert to old G-tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 const GOOGLE_TAG_MANAGER_ID = 'GTM-MSXRDQJQ'
 
@@ -10,11 +9,11 @@ export default function Document() {
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer', '${GOOGLE_TAG_MANAGER_ID}');
             `,
 					}}
 				/>
