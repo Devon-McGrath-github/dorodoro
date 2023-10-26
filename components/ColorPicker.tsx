d64238a58f9d96d48f2ee13b5e9f709621718bba
Devon McGrath <devon.mcgrath@gmail.com>
2023-10-27 11:24:37 +1300

    Use popover for ColorPicker component

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,25 +1,41 @@
 import { useStore } from '@/store/Timer'
-import React, { useCallback, useRef, useState } from 'react'
+import React, { Fragment } from 'react'
 import { HexColorPicker, HexColorInput } from 'react-colorful'
-import useClickOutside from '@/hooks/useClickOutside'
+import { Popover, Transition } from '@headlessui/react'
 
 function ColorPicker() {
 	const [color, updateColor] = useStore((s) => [s.color, s.updateColor])
-	const [isOpen, setIsOpen] = useState(false)
-
-	const popover = useRef<HTMLDivElement | null>(null)
-	const close = useCallback(() => setIsOpen(false), [])
-	useClickOutside(popover, close)
 
 	return (
 		<>
-			<div
-				id='color_picker'
-				className='w-10 h-10 rounded-full border-2 border-solid border-white bg-skin-primary cursor-pointer'
-				onClick={() => setIsOpen(true)}
-			></div>
-
-			{isOpen && (
+			<Popover className='relative'>
+				{({ open }) => (
+					<>
+						<Popover.Button
+							id='color_picker'
+							className='w-10 h-10 rounded-full border-2 border-solid border-white bg-skin-primary cursor-pointer'
+						></Popover.Button>
+						<Transition
+							as={Fragment}
+							enter='transition ease-out duration-200'
+							enterFrom='opacity-0 translate-y-1'
+							enterTo='opacity-100 translate-y-0'
+							leave='transition ease-in duration-150'
+							leaveFrom='opacity-100 translate-y-0'
+							leaveTo='opacity-0 translate-y-1'
+						>
+							<Popover.Panel className='absolute left-1/2 z-10 mt-2 -translate-x-[90%] transform'>
+								<HexColorPicker
+									className='cursor-pointer'
+									color={color}
+									onChange={updateColor}
+								/>
+							</Popover.Panel>
+						</Transition>
+					</>
+				)}
+			</Popover>
+			{/* {isOpen && (
 				<div className='absolute' ref={popover}>
 					<HexColorPicker
 						className='cursor-pointer'
@@ -27,7 +43,7 @@ function ColorPicker() {
 						onChange={updateColor}
 					/>
 				</div>
-			)}
+			)} */}
 
 			{/* <HexColorInput
 				color={color}
