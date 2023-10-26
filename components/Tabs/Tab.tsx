8759acbc04f954596b6914cf328520a1f0d5725c
Devon McGrath <devon.mcgrath@gmail.com>
2023-10-27 11:51:23 +1300

    Make tabs focusable

--- components/Tabs/Tab.tsx
+++ components/Tabs/Tab.tsx
@@ -14,7 +14,7 @@ const Tab = ({ tabName, timerType }: TabProps) => {
 	])
 
 	return (
-		<li className='flex-1' role='tab'>
+		<li className='flex-1' role='tab' tabIndex={0}>
 			<div
 				className={
 					'h-full align-middle text-center items-stretch text-xs cursor-pointer font-bold uppercase px-3 py-2 rounded bg-skin-primary box-border border border-transparent hover:border hover:border-white hover:bg-opacity-50 focus:ring-gray-200 ' +
